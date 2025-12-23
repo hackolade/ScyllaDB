7aa3b60b2b850296733f33f5ad49c9906f9e2663
Serhii Filonenko <serhii.filonenko@binary-studio.com>
2025-12-23 15:42:02 +0200

    HCK-14120: fix the generation of an alter script for pk/ck if new values ​​do not exist

--- forward_engineering/helpers/updateHelpers/tableHelper.js
+++ forward_engineering/helpers/updateHelpers/tableHelper.js
@@ -132,11 +132,6 @@ const hydrateColumn = ({ tableName, keyspaceName, isOldModel, property, udtMap, 
 	};
 };
 
-const getTableParameter = (item, key) => {
-	const parameter = item?.role?.compMod?.[key];
-	return parameter?.new || item?.role?.[key];
-};
-
 const addToKeysHashType = (keysHash, keys) => {
 	return Object.entries(keysHash).reduce((keysHash, [id, key]) => {
 		const type = (keys.find(key => key.keyId === id) || {}).type;
@@ -229,8 +224,8 @@ const getAddTable = addTableData => {
 	}
 	let table = addTableData.item;
 	const data = addTableData.data;
-	const compositePartitionKey = getTableParameter(table, 'compositePartitionKey') || [];
-	const compositeClusteringKey = getTableParameter(table, 'compositeClusteringKey') || [];
+	const compositePartitionKey = table?.role?.compMod?.compositePartitionKey?.new || [];
+	const compositeClusteringKey = table?.role?.compMod?.compositeClusteringKey?.new || [];
 
 	const entityData = [
 		{
@@ -270,7 +265,6 @@ module.exports = {
 	alterTablePrefix,
 	hydrateColumn,
 	isTableChange,
-	getTableParameter,
 	addScriptToExistScripts,
 	getDeleteTable,
 	getAddTable,
